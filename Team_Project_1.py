d17168a4e418f80928eaf72c585e4c7575ce32e5
AbdullahAlsulami <aalsulami1270@stu.kau.edu.sa>
2024-11-22 21:34:55 +0300

    Update Team_Project_1.py
    Abdullah edited the code

--- Team_Project_1.py
+++ Team_Project_1.py
@@ -1,7 +1,15 @@
 # import must be written here
 
 # Ship Class
+class Ship:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+        self.coordinates = []
+        self.hits = 0
 
+    def is_sunk(self):
+        return self.hits == self.size
 # Player Class
 
 # AI Player Class
@@ -79,3 +87,77 @@ class BattleshipGame(QMainWindow):
 
     def initUI(self):
         pass
+#  StartWindow(QMainWindow) class
+
+
+#   PlacementWindow class 
+
+
+
+
+# Manual Placement Window
+class ManualPlacementWindow(QMainWindow):
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.setWindowTitle("Manual Placement")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.current_ship_index = 0
+        self.direction = 0
+        self.initUI()
+
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        self.ship_label = QLabel(f"Place {ship_list[self.current_ship_index].name}")
+        self.ship_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.ship_label)
+
+        self.grid = QGridLayout()
+        self.buttons = [[QPushButton("~") for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+        for i in range(BOARD_SIZE):
+            for j in range(BOARD_SIZE):
+                self.buttons[i][j].setFixedSize(30, 30)
+                self.buttons[i][j].clicked.connect(lambda _, r=i, c=j: self.place_ship(r, c))
+                self.grid.addWidget(self.buttons[i][j], i, j)
+        layout.addLayout(self.grid)
+
+        direction_layout = QHBoxLayout()
+        self.horizontal_button = QRadioButton("Horizontal")
+        self.horizontal_button.setChecked(True)
+        self.horizontal_button.toggled.connect(self.set_horizontal)
+        direction_layout.addWidget(self.horizontal_button)
+
+        self.vertical_button = QRadioButton("Vertical")
+        self.vertical_button.toggled.connect(self.set_vertical)
+        direction_layout.addWidget(self.vertical_button)
+        layout.addLayout(direction_layout)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def set_horizontal(self):
+        self.direction = 0
+
+    def set_vertical(self):
+        self.direction = 1
+
+    def place_ship(self, row, col):
+        ship = ship_list[self.current_ship_index]
+        if self.game.player.is_space_available(row, col, ship.size, self.direction):
+            self.game.player.place_ship(ship, row, col, self.direction)
+            for r, c in ship.coordinates:
+                self.buttons[r][c].setText("X")
+                self.buttons[r][c].setStyleSheet("background-color: gray")
+            self.current_ship_index += 1
+            if self.current_ship_index < len(ship_list):
+                self.ship_label.setText(f"Place {ship_list[self.current_ship_index].name}")
+            else:
+                self.close()
+                self.battle_window = BattleWindow(self.game)
+                self.battle_window.show()
+
+
+# Battle Window
